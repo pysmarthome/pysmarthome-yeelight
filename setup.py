ce808670f77b4e12f1976411cd3df48b981eebe0
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-09-11 16:43:53 -0300

    setup: update to version 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pysmarthome-yeelight',
     description='Yeelight plugin for pysmarthome',
-    version='1.0.2',
+    version='1.1.0',
     author='Filipe Alves',
     author_email='filipe.alvesdefernando@gmail.com',
     install_requires=[
