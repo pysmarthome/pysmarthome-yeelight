5e348d7d9e5aa67e218ed2b6e7c288056df211da
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-09-11 16:50:57 -0300

    setup: fix: pysmarthome ~= 2.3 is required to set_color

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='pysmarthome-yeelight',
     description='Yeelight plugin for pysmarthome',
-    version='1.1.0',
+    version='1.1.1',
     author='Filipe Alves',
     author_email='filipe.alvesdefernando@gmail.com',
     install_requires=[
-        'pysmarthome~=2.2',
+        'pysmarthome~=2.3',
         'yeelight',
     ],
     packages=find_packages(),
